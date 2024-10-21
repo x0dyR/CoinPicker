7b0e513e2aee064c8cb0c7a7c96e9a6e0acd8b0c
x0dyR <manoshki.dan4ik@gmail.com>
2024-10-21 13:35:32 +0500

    Added rotation to coins
    Added rotation(can be applied to anyone) coins from code by transfrom.Rotate()

---
+++ Assets/_Scripts/ObjectRotator.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ObjectRotator : MonoBehaviour
+{
+    [SerializeField] private float _angle;
+    
+    private void Update()
+        => transform.Rotate(Vector3.up, _angle * Time.deltaTime);
+}
